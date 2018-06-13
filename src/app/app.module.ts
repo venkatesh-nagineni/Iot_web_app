7300ab04d0025289d60698b11fe4b9d0091b88ab
Venkatesh <venky.chowdary91@gmail.com>
2018-06-13 11:22:35 +0200

    updates to deploy to heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { PubNubAngular } from 'pubnub-angular2';
 import { AppComponent } from './app.component';
+import { ChartModule } from 'angular2-highcharts';
+import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
+
+declare var require: any;
+export function highchartsFactory() {
+  const hc = require('highcharts/highstock');
+    const hcm = require('highcharts/highcharts-more');
+    hcm(hc);
+    return hc;
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartModule
   ],
-  providers: [],
+  providers: [PubNubAngular, { provide: HighchartsStatic, useFactory: highchartsFactory}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
